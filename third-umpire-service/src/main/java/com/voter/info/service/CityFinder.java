a0480b03d3257eda751383b49b63a45ecd4b1cf5
Shyam <catch.shyambaitmangalkar@gmail.com>
2018-02-25 23:59:10 +0530

    Adding webclient to fetch cities

--- third-umpire-service/src/main/java/com/voter/info/service/CityFinder.java
+++ third-umpire-service/src/main/java/com/voter/info/service/CityFinder.java
@@ -2,9 +2,13 @@ package com.voter.info.service;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 /**
  * 
@@ -26,5 +30,22 @@ public class CityFinder {
 	public static void findAllCities() {
 		WebClient client = new WebClient();
 		
+		client.getOptions().setThrowExceptionOnFailingStatusCode(false);
+		client.getOptions().setCssEnabled(false);
+		client.getOptions().setJavaScriptEnabled(true);
+		
+		try {
+			String draftRoolURL = getProperties().getProperty("DRAFT_ROLL_URL").replace("%d", "2016");
+			HtmlPage draftRoolPage = client.getPage(draftRoolURL);
+			List<HtmlAnchor> anchors = draftRoolPage.getAnchors();
+			anchors.forEach(e -> System.out.println(e.asXml()));
+		} catch (FailingHttpStatusCodeException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		findAllCities();
 	}
 }
