904422acaf4b766004791c71bf34dfc9c75f4df8
Shyam <shyam.baitmangalkar@thomsonreuters.com>
2018-03-01 20:19:47 +0530

    Adding a working logic that finds a person in given assembly constituency

--- third-umpire-service/src/main/java/com/voter/info/service/PersonFinder.java
+++ third-umpire-service/src/main/java/com/voter/info/service/PersonFinder.java
@@ -2,19 +2,34 @@ package com.voter.info.service;
 
 import java.util.List;
 
+import com.voter.info.model.UserRequest;
 import com.voter.info.model.Voter;
+import com.voter.info.service.PDFParser;
 
 public class PersonFinder {
-	public static Voter findPerson(String name, String districtName) {
+	public static Voter findPerson(String firstName, String middleName, String lastName, String districtName) {
 		//TODO: Implement findPerson(String name) method
 		
 		return null;
 	}
 	
-	public static Voter findPerson(String name, String districtName, String assemblyConstituencyName) {
+	public static Voter findPerson(String firstName,String middleName, String lastName, String districtName, String assemblyConstituencyName) {
 		//TODO: Implement findPerson(String name, String assemblyConstituencyName) method
 		String assemblyConstituencyURL = AssemblyConstituencyFinder.getURLForAssemblyConstituency(districtName, assemblyConstituencyName);
 		List<String> allPollingStationURLs = PollingStationFinder.findAllPollingStationsURL(assemblyConstituencyURL);
+		allPollingStationURLs.parallelStream()
+		                     .map(url -> PDFParser.partialParse(url, firstName, lastName))
+		                     .filter(res -> res.getResult().contains(lastName))
+		                     .forEach(System.out::println);
 		return null;
+	}
+	public static void main(String[] args) {
+		VoterService vs = new VoterServiceImpl();
+		UserRequest req = new UserRequest();
+		req.setFirstName("");
+		req.setLastName("");
+		req.setDistrict("");
+		req.setAssemblyConstituencyName("");
+		vs.findVoter(req);
 	}
 }
