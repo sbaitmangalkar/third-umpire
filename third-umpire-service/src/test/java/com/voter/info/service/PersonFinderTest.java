698f90fee6c7dc4548c98e3f56d0e567f01e5f95
Shyam <shyam.baitmangalkar@thomsonreuters.com>
2018-03-07 21:03:55 +0530

    Adding test method to test fetch by district name functionality

--- third-umpire-service/src/test/java/com/voter/info/service/PersonFinderTest.java
+++ third-umpire-service/src/test/java/com/voter/info/service/PersonFinderTest.java
@@ -3,25 +3,44 @@ package com.voter.info.service;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Test class for <code>PersonFinder</code>
+ * 
+ * @author Shyam | catch.shyambaitmangalkar@gmail.com
+ *
+ */
 public class PersonFinderTest {
 	private String firstName;
 	private String middleName;
 	private String lastName;
 	private String districtName;
 	private String assemblyConstituencyName;
-	
+
 	@Before
 	public void init() {
-		firstName = "Anusha";
-		lastName = "Malagi";
+		firstName = "Kishan";
+		lastName = "Kumar";
 		districtName = "B.B.M.P(NORTH)";
 		assemblyConstituencyName = "Mahalakshmi Layout";
-		
 	}
-	
+
 	@Test
-	public void testFor() {
-		//TODO: findPerson method is not completely ready for use/test. 
+	public void findPersonInAnAssemblyConstituency() {
+		long startTime = System.currentTimeMillis();
 		PersonFinder.findPerson(firstName, middleName, lastName, districtName, assemblyConstituencyName);
+		long endTime = System.currentTimeMillis();
+
+		long totaltime = ((endTime - startTime) / 1000) / 60;
+		System.out.println("Time taken to crawl entire assembly constituency -> " + totaltime + " min(s).");
+	}
+
+	@Test
+	public void findPersonInDistrict() {
+		long startTime = System.currentTimeMillis();
+		PersonFinder.findPerson(firstName, middleName, lastName, districtName);
+		long endTime = System.currentTimeMillis();
+
+		long totaltime = ((endTime - startTime) / 1000) / 60;
+		System.out.println("Time taken to crawl entire district records -> " + totaltime + " min(s).");
 	}
 }
