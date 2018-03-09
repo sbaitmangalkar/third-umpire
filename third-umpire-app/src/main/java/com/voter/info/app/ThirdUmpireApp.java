1c13b1cb0e1638c026c9cb270428682f8e9c5e85
Shyam <shyam.baitmangalkar@thomsonreuters.com>
2018-03-09 20:21:21 +0530

    Adding combo box to select district and assembly constituency

--- third-umpire-app/src/main/java/com/voter/info/app/ThirdUmpireApp.java
+++ third-umpire-app/src/main/java/com/voter/info/app/ThirdUmpireApp.java
@@ -1,19 +1,61 @@
 package com.voter.info.app;
 
+import java.net.ConnectException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import com.voter.info.service.VoterService;
+import com.voter.info.service.VoterServiceImpl;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.stage.Stage;
-import javafx.scene.layout.*;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class ThirdUmpireApp extends Application {
+	private VoterService voterService;
+	private List<String> allDistricts;
+	private Map<String, List<String>> districtsWithAssemblyConstituencies;
+	
+	private void initialize() {
+		voterService = new VoterServiceImpl();
+		districtsWithAssemblyConstituencies = voterService.getDistrictsWithAssemblyConstituencies();
+		allDistricts = districtsWithAssemblyConstituencies.entrySet()
+				                                          .stream()
+				                                          .map(entry -> entry.getKey())
+				                                          .collect(Collectors.toList());            
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Third Umpire");
+		GridPane grid = new GridPane();
+		/*try {
+			initialize();
+		} catch(Exception e) {
+			grid.add(new Label("Oops!! Remote server is unreachable. Please try after some time."), 0, 0);
+			Button closeButton = new Button("Close");
+			closeButton.setOnAction(value -> System.exit(0));
+			grid.add(closeButton, 0, 1);
+			Scene scene = new Scene(new Group(), 450,250);
+			
+			Group root = (Group)scene.getRoot();
+			root.getChildren().add(grid);
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}*/
+		Scene scene = new Scene(new Group(), 450,250);
+		
 		TextField firstNameTextField = new TextField();
 		firstNameTextField.setPromptText("First Name");
 		
@@ -24,29 +66,52 @@ public class ThirdUmpireApp extends Application {
 		TextField lastNameTextField = new TextField();
 		lastNameTextField.setPromptText("Last Name");
 		
-		TextField districtNameTextField = new TextField();
-		districtNameTextField.setPromptText("District Name");
+		ObservableList<String> districtOptions = FXCollections.observableArrayList(allDistricts);
+		ComboBox<?> districtComboBox = new ComboBox<>(districtOptions);
 		
-		TextField assemblyConstituencyNameTextField = new TextField();
-		assemblyConstituencyNameTextField.setPromptText("Assembly Constituency Name");
+		districtComboBox.setOnAction(value -> {
+			String selectedDistrict = (String)districtComboBox.getValue();
+			ComboBox<?> assemblyConstituencyComboBox = null;
+			if(selectedDistrict != null && selectedDistrict.length() > 1) {
+				if(grid.getChildren().contains(assemblyConstituencyComboBox))
+					grid.getChildren().remove(assemblyConstituencyComboBox);
+				ObservableList<String> assemblyConstituencyOptions = FXCollections.observableArrayList(getAllAssemblyConstituencies(selectedDistrict));
+				assemblyConstituencyComboBox = new ComboBox<>(assemblyConstituencyOptions);
+				
+				
+				grid.add(assemblyConstituencyComboBox, 1, 4);
+			}
+		});
 		
-		GridPane root = new GridPane();
-		root.setPadding(new Insets(10, 10, 10, 10));
-		root.setVgap(5);
-		root.setHgap(5);
-		GridPane.setConstraints(firstNameTextField, 0, 0);
-		root.getChildren().add(firstNameTextField);
-		GridPane.setConstraints(middleNameTextField, 0, 1);
-		root.getChildren().add(middleNameTextField);
-		GridPane.setConstraints(lastNameTextField, 0, 2);
-		root.getChildren().add(lastNameTextField);
-		GridPane.setConstraints(districtNameTextField, 0, 3);
-		root.getChildren().add(districtNameTextField);
-		GridPane.setConstraints(assemblyConstituencyNameTextField, 0, 4);
-		root.getChildren().add(assemblyConstituencyNameTextField);
 		
-		primaryStage.setScene(new Scene(root, 300, 300));
+		
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(5);
+		grid.setHgap(5);
+		
+		grid.add(new Label("First Name:"), 0, 0);
+		grid.add(firstNameTextField, 1, 0);
+		grid.add(new Label("Middle Name:"), 0, 1);
+		grid.add(middleNameTextField, 1, 1);
+		grid.add(new Label("Last Name:"), 0, 2);
+		grid.add(lastNameTextField, 1, 2);
+		
+		grid.add(new Label("Districts:"), 0, 3);
+		grid.add(districtComboBox, 1, 3);
+		grid.add(new Label("Assembly Constituencies:"), 0, 4);
+		
+		Group root = (Group)scene.getRoot();
+		root.getChildren().add(grid);
+		
+		
+		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+	
+	
+	private List<String> getAllAssemblyConstituencies(String districtName) {
+		return districtsWithAssemblyConstituencies.get(districtName);
 	}
 	
 	public static void main(String[] args) {
