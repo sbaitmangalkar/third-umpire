62f7a50886a2621374c988cc57edeebcd0c6653d
Shyam <catch.shyambaitmangalkar@gmail.com>
2018-03-11 13:55:25 +0530

    Adding Sorter which helps to sort assembly constituency names

---
+++ third-umpire-app/src/main/java/com/voter/info/app/AssemblyConstituencySorter.java
@@ -0,0 +1,22 @@
+package com.voter.info.app;
+
+import java.util.Comparator;
+
+/**
+ * 
+ * @author Shyam | @sbaitmangalkar| catch.shyambaitmangalkar@gmail.com
+ *
+ */
+public class AssemblyConstituencySorter implements Comparator<String> {
+
+	@Override
+	public int compare(String assemblyConstituency1, String assemblyConstituency2) {
+		if (assemblyConstituency1.compareTo(assemblyConstituency2) > 0)
+			return 1;
+		else if (assemblyConstituency1.compareTo(assemblyConstituency2) < 0)
+			return -1;
+		else
+			return 0;
+	}
+
+}
